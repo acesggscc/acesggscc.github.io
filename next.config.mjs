c13ce0e2e1e9d57b8d5e79fe6d002d77021a6e81
shaurya gupta <119103602+shauryagupta3@users.noreply.github.com>
2024-02-20 00:09:41 +0530

    changed dist dir

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     basePath:"/acesggscc.github.io",
+    distDir:"./dist",
     output:"export",
     images:{
         unoptimized:true,
