56db9c14c1d08bc995edbdc2df0a97f5916ee3da
acesggscc <160531254+acesggscc@users.noreply.github.com>
2024-02-20 00:22:57 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath:"/acesggscc.github.io",
+    // basePath:"/acesggscc.github.io",
     distDir:"dist",
     output:"export",
     images:{
