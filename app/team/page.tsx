af7eba6d7bd35615d6c65fdbff9f2c25ac64d488
shaurya gupta <119103602+shauryagupta3@users.noreply.github.com>
2024-03-04 14:15:53 +0530

    added templates for other pages

---
+++ app/team/page.tsx
@@ -0,0 +1,16 @@
+export default function Home() {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-between">
+        <section
+          id="hero"
+          className="font-bold text-center w-screen flex flex-col justify-around items-center"
+        >
+          <h1 className="text-gray-300 p-20 text-6xl md:p-40 md:text-8xl">
+            TEAM
+            <span className="text-cyan-500">.</span>
+          </h1>
+        </section>
+      </main>
+    );
+  }
+  
